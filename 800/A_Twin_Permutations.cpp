a720434d0f433d70b82bde969e0037c14441197b
Anandkumar Gupta <78360810+Anandkumar85@users.noreply.github.com>
2026-07-11 17:22:07 +0530

    A_Twin_Permutations

--- 800/A_Twin_Permutations.cpp
+++ 800/A_Twin_Permutations.cpp
@@ -11,13 +11,14 @@ int main()
         cin >> n; 
         long long a[n]; 
         for (int i = 0; i < n; i++) 
-            cin >> a[i];
-
+            cin >> a[i]; 
         for (int i = 0; i < n; i++) 
         {
+          
             cout << n + 1 - a[i] << " ";
         }
         cout << endl; 
     }
     return 0; 
 }
+
